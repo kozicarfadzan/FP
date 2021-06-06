da8e99977a8341ca9a4e3401f9aedddfac86d054
Lejla Kozić <lejla.kozic@edu.fit.ba>
2021-06-06 15:47:07 +0200

    Migracija za BiciklStanje.Kolicina, brisanje KupacId atributa

--- FahrradladenPrinzenstrasse.Data/Migrations/20210606124548_biciklstanje_kolicina.Designer.cs
+++ FahrradladenPrinzenstrasse.Data/Migrations/20210606124548_biciklstanje_kolicina.Designer.cs
@@ -4,14 +4,16 @@ using FahrradladenPrinzenstrasse.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FahrradladenPrinzenstrasse.Data.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20210606124548_biciklstanje_kolicina")]
+    partial class biciklstanje_kolicina
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FahrradladenPrinzenstrasse.Data/Migrations/20210606124548_biciklstanje_kolicina.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FahrradladenPrinzenstrasse.Data.Migrations
+{
+    public partial class biciklstanje_kolicina : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BiciklStanje_Klijent_KupacId",
+                table: "BiciklStanje");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BiciklStanje_KupacId",
+                table: "BiciklStanje");
+
+            migrationBuilder.DropColumn(
+                name: "KupacId",
+                table: "BiciklStanje");
+
+            migrationBuilder.DropColumn(
+                name: "Sifra",
+                table: "BiciklStanje");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Kolicina",
+                table: "BiciklStanje",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Kolicina",
+                table: "BiciklStanje");
+
+            migrationBuilder.AddColumn<int>(
+                name: "KupacId",
+                table: "BiciklStanje",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sifra",
+                table: "BiciklStanje",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BiciklStanje_KupacId",
+                table: "BiciklStanje",
+                column: "KupacId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BiciklStanje_Klijent_KupacId",
+                table: "BiciklStanje",
+                column: "KupacId",
+                principalTable: "Klijent",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
